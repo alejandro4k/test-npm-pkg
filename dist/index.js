1177385583ea6c1c1595451415fe3d27d5c9d160
alejandro4k <jhoanalejandro.anaya@gmail.com>
2022-09-28 12:29:29 -0500

    add ts config

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getCurrentDate = void 0;
+const getCurrentDate = () => {
+    return new Date().toISOString();
+};
+exports.getCurrentDate = getCurrentDate;
+//# sourceMappingURL=index.js.map
